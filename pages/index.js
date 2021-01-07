dc076b1aa20232ee4a73585025040c461de1b56f
Martín M <skydiver@users.noreply.github.com>
2021-01-06 23:54:42 -0300

    applied nextjs preset

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <>
+    <Head>
+      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div>Next.js + Tailwind CSS</div>
+    </div>
+  </>
+);
+
+export default Home;
