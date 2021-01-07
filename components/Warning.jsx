9f6cffa898f3ba163020ef692809f3e47c8c9881
Martín M <skydiver@users.noreply.github.com>
2021-01-07 00:54:33 -0300

    creating paypal example

---
+++ components/Warning.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Warning = ({ message, dismiss }) => (
+  <div className="w-full mb-8 bg-yellow-50 border-l-4 border-yellow-400 p-4">
+    <div className="flex">
+      <div className="flex-shrink-0">
+        <svg
+          className="h-5 w-5 text-yellow-400"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          aria-hidden="true">
+          <path
+            fillRule="evenodd"
+            d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+      <div className="ml-3">
+        <p className="text-sm text-yellow-700">{message}</p>
+      </div>
+      <div className="ml-auto pl-3">
+        <div className="-mx-1.5 -my-1.5">
+          <button
+            className="inline-flex bg-yellow-50 rounded-md p-1.5 text-yellow-500 hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-yellow-50 focus:ring-yellow-600"
+            onClick={dismiss}>
+            <span className="sr-only">Dismiss</span>
+            <svg
+              className="h-5 w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true">
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Warning;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,67 @@
-import Head from 'next/head';
+import { useState } from 'react';
+import { PayPalScriptProvider, PayPalButtons, FUNDING } from '@paypal/react-paypal-js';
 
-const Home = () => (
-  <>
-    <Head>
-      <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-    </Head>
-    <div className="w-screen h-screen flex items-center justify-center">
-      <div>Next.js + Tailwind CSS</div>
+import Warning from '../components/Warning';
+
+const Home = () => {
+  const [cancelled, setCancelled] = useState(false);
+  const [orderDetails, setOrderDetails] = useState(null);
+
+  const createOrder = (data, actions) => {
+    return actions.order
+      .create({
+        application_context: {
+          shipping_preference: 'NO_SHIPPING'
+        },
+        purchase_units: [
+          {
+            amount: {
+              currency: 'USD',
+              value: 10
+            }
+          }
+        ]
+      })
+      .then((orderID) => {
+        return orderID;
+      });
+  };
+
+  const onApprove = (data) => {
+    setOrderDetails(data);
+  };
+
+  const onCancel = () => {
+    setCancelled(true);
+  };
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center justify-center">
+      <div className="w-1/4">
+        {cancelled && <Warning message="Order cancelled!" dismiss={() => setCancelled(false)} />}
+
+        {orderDetails && (
+          <pre className="absolute top-0 right-0 w-1/3 bg-gray-200 border-2 border-gray-500 overflow-scroll">
+            <h2 className="mb-4 font-semibold">Order Result:</h2>
+            {JSON.stringify(orderDetails, null, 2)}
+          </pre>
+        )}
+
+        <PayPalScriptProvider
+          options={{
+            'client-id':
+              'ARZFn9joxeTaOb8GLoJPx5LmG4RbX7CyaKjFD0FVbHSc1oixI7T4Q0tzBCNHgFwf1r2PapXus51c6lQY'
+          }}>
+          <PayPalButtons
+            fundingSource={FUNDING.PAYPAL}
+            createOrder={createOrder}
+            onApprove={onApprove}
+            onCancel={onCancel}
+          />
+        </PayPalScriptProvider>
+      </div>
     </div>
-  </>
-);
+  );
+};
 
 export default Home;
